0995225c8cb59542a86ae2035358828dfbcfe0f9
Faraz Shaikh <farazzshaikh@gmail.com>
2022-03-10 01:18:51 +0530

    Update deps

--- examples/layer-materials/src/App.js
+++ examples/layer-materials/src/App.js
@@ -72,7 +72,6 @@ function Flower({ base, colorA, colorB }) {
         />
         <Depth ref={depth} near={0.25} far={2} colorA={'#ffe100'} alpha={0.5} mode={'lighten'} mapping={'vector'} />
         <Fresnel mode={'softlight'} />
-        <Normal />
       </DebugLayerMaterial>
     </mesh>
   )
