4aea4a5f765991fa5e87341a39c9d0a4d4303662
Faraz Shaikh <farazzshaikh@gmail.com>
2022-04-30 00:48:09 +0530

    chore: bump CSM

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,13 +1,11 @@
+rm -rf ./node_modeules ./yarn.lock
+yarn
 
 cd dist
 yarn link
 cd ../
 
-
 cd ./node_modules/react
-yarn link
-cd ../../
-cd ./node_modules/react-dom
 yarn link
 cd ../../
 cd ./node_modules/three
@@ -17,12 +15,25 @@ cd ./node_modules/@react-three/fiber
 yarn link
 cd ../../../
 
-for d in ./examples/* ; do
-    cd $d
+# for d in ./examples/* ; do
+#     cd $d
+#     rm -rf ./node_modeules ./yarn.lock
+#     yarn
+
+#     yarn link three-custom-shader-material
+#     yarn link react
+#     yarn link three
+#     yarn link @react-three/fiber
+#     cd ../../
+# done
+
+    cd ./examples/example-configurator
+    rm -rf ./node_modeules ./yarn.lock
+    yarn
+
     yarn link lamina
     yarn link react
     yarn link react-dom
     yarn link three
     yarn link @react-three/fiber
     cd ../../
-done
